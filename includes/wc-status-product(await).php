177bb2d22ce90bdae5b99c37daeab4d74e78052f
sslGS <gscs355@gmail.com>
2021-12-24 16:39:16 +0200

    includes/wc-status-product(await).php

--- includes/wc-status-product(await).php
+++ includes/wc-status-product(await).php
@@ -2,39 +2,9 @@
 
 if(!defined('ABSPATH')) exit;
 
-add_filter( 'woocommerce_product_stock_status_options', 'filter_woocommerce_product_stock_status_options', 10, 1 );
-function filter_woocommerce_product_stock_status_options( $status ) {
-    $status['await'] = "Ожидается";
-
-    return $status;
-}
-
-add_filter('woocommerce_is_purchasable', 'filter_is_purchasable_callback', 10, 2 );
-add_filter('woocommerce_variation_is_purchasable', 'filter_is_purchasable_callback', 10, 2 );
-function filter_is_purchasable_callback( $purchasable, $product ) {
-    if ( $product->get_stock_status() === 'await' ) {
-        return false;
-    }
-
-    return $purchasable;
-}
-
-add_filter( 'woocommerce_get_availability_text', 'filter_woocommerce_get_availability_text', 10, 2 );
-function filter_woocommerce_get_availability_text( $availability, $product ) {
-    $order_id = $product->get_id();
-    switch( $product->get_stock_status() ) {
-        case 'await':
-            $text = get_post_meta ( $order_id, "_await_text", true);
-            $availability = "<p class='stock out-of-stock'>Ожидается: {$text}</p>";
-        break;
-    }
-
-    return $availability; 
-}
-
 add_filter( 'woocommerce_get_availability_class', 'filter_woocommerce_get_availability_class', 10, 2 );
-function filter_woocommerce_get_availability_class( $class, $product ) {
-    switch( $product->get_stock_status() ) {
+function filter_woocommerce_get_availability_class($class, $product) {
+    switch($product->get_stock_status()) {
         case 'await':
             $class = 'await'; 
         break;
@@ -43,15 +13,43 @@ function filter_woocommerce_get_availability_class( $class, $product ) {
     return $class;
 }
 
-add_filter( 'woocommerce_admin_stock_html', 'filter_woocommerce_admin_stock_html', 10, 2 );
-function filter_woocommerce_admin_stock_html( $stock_html, $product ) {
+add_filter( 'woocommerce_product_stock_status_options', 'filter_woocommerce_product_stock_status_options', 10, 1 );
+function filter_woocommerce_product_stock_status_options($status) {
+    $status['await'] = "Ожидается";
+
+    return $status;
+}
+
+add_filter('woocommerce_admin_stock_html', 'filter_woocommerce_admin_stock_html', 10, 2);
+function filter_woocommerce_admin_stock_html($stock_html, $product) {
     $order_id = $product->get_id();
-    switch( $product->get_stock_status() ) {
+    switch($product->get_stock_status()) {
         case 'await':
-            $text = get_post_meta ( $order_id, "_await_text", true);
+            $text = get_post_meta ($order_id, "_await_text", true);
             $stock_html = "<mark class='outofstock'>Ожидается: {$text}</mark>"; 
         break;
     }   
 
     return $stock_html;
+}
+
+add_filter('woocommerce_is_purchasable', 'check_purchasable_product', 10, 2 );
+function check_purchasable_product($is_purchasable, $product) {
+	if( $product->get_stock_status() === 'await' ) {
+		return false;
+	}
+	return $is_purchasable;
+}
+
+add_action( 'woocommerce_single_product_summary', 'unavailable_product_display_message', 20 );
+function unavailable_product_display_message() {
+    global $product;
+    $order_id = $product->get_id();
+    switch( $product->get_stock_status() ) {
+        case 'await':
+            $text = get_post_meta ($order_id, "_await_text", true);
+            $availability = "<p class='stock out-of-stock'>Ожидается: {$text}</p>";
+            echo $availability;
+        break;
+    }
 }
